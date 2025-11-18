9002774cad5cfefb4450ec41c2cce738eb66d179
Elchkacke <sebastian.kleffner@gmail.com>
2025-11-18 09:17:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
-// Lehrer-PIN (bitte anpassen)
 const TEACHER_PIN = "1234";
 
-// Server-URL für Ergebnis-Speicherung.
-// Wenn du keinen Server hast: Dummy-URL lassen.
 const SERVER_URL = "https://script.google.com/macros/s/AKfycbx1Fp2z4frJxAOjR1qUctCHsQb7pxIipNvIGvYlXqxwLKeOE2UbPyrQZqkS20_BXzsSEA/exec";
 
-// Start-Konfiguration (überschreibt die Default-Werte aus index.html)
 window.SERVER_CONFIG = {
   count: 10,
   min: 1,
